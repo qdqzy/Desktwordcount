01bb4bc90e2d0dfeb3a61823eabff8cd01e12eb1
tony <876138262@qq.com>
2019-06-17 16:02:35 +0800

    词频统计功能

--- Desktwordcount/views.py
+++ Desktwordcount/views.py
@@ -2,3 +2,17 @@ from django.http import  HttpResponse
 from django.shortcuts import render
 def home(request):
     return render(request, 'home.html')
+
+def count(request):
+    text = request.GET['text']
+    print(text)
+
+    result = {}
+    for i in text:
+        if i not in result:
+            result[i] = 1
+        else:
+            result[i] += 1
+    result = sorted(result.item(), key = lambda x:x[1], reverse = True)
+
+    return render(request,'count.html',{'count_result':result.items()})
